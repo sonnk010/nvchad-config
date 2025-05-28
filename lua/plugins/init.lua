9bdbcf2f48ef5eeb9f8af00a9bef25112ec0303f
sonnk.010 <sonnk.010@gmail.com>
2025-05-29 00:07:29 +0700

    Add project nvim, auto session

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,13 +16,49 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup({
+        datadir = vim.fn.stdpath("data") .. "/site/projects",
+        detection_methods = { "lsp", "pattern" },
+        patterns = { ".git", "Makefile", "package.json", ".project" },
+        exclude_dirs = {},
+        change_detection_methods = { "lsp", "pattern" },
+      })
+    end,
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Desktop', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+    config = function()
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+      require('auto-session').setup {
+        log_level = 'info',
+        auto_session_enable_last_session = false,
+        auto_session = true,
+        auto_save = true,
+        auto_restore = true,
+        lazy_support = true,
+        session_lens = { load_on_setup = true },
+      }
+    end,
+  }
 }
