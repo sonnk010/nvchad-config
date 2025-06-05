b92f3ae3e25e9e9d23e070a6f55c8b5264344a03
sonnk.010 <sonnk.010@gmail.com>
2025-06-05 15:43:59 +0700

    remove auto session

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,8 +18,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css"
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
       },
     },
   },
@@ -27,45 +30,21 @@ return {
     "ahmedkhalf/project.nvim",
     lazy = false,
     config = function()
-      require("project_nvim").setup({
-        datadir = vim.fn.stdpath("data") .. "/site/projects",
+      require("project_nvim").setup {
+        datadir = vim.fn.stdpath "data" .. "/site/projects",
         detection_methods = { "lsp", "pattern" },
         patterns = { ".git", "Makefile", "package.json", ".project" },
         exclude_dirs = {},
         change_detection_methods = { "lsp", "pattern" },
-      })
-      require("telescope").load_extension("projects")
-    end,
-  },
-  {
-    'rmagatti/auto-session',
-    lazy = false,
-
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { '~/', '~/Desktop', '~/Downloads', '/' },
-      -- log_level = 'debug',
-    },
-    config = function()
-      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-      require('auto-session').setup {
-        log_level = 'info',
-        auto_session_enable_last_session = false,
-        auto_session = true,
-        auto_save = true,
-        auto_restore = true,
-        lazy_support = true,
-        session_lens = { load_on_setup = true },
       }
+      require("telescope").load_extension "projects"
     end,
   },
   {
     "petertriho/nvim-scrollbar",
     lazy = false,
     config = function()
-      require("scrollbar").setup({
+      require("scrollbar").setup {
         show = true,
         hide_if_all_visible = true,
         set_highlight = true,
@@ -76,8 +55,8 @@ return {
           handle = true,
           search = true, -- Requires hlslens
           ale = false, -- Requires ALE tb
-        }
-      })
+        },
+      }
     end,
   },
   {
@@ -85,22 +64,22 @@ return {
     lazy = false,
     event = "BufRead",
     config = function()
-      require('gitsigns').setup {
+      require("gitsigns").setup {
         signs = {
-          add = { text = '┃' },
-          change = { text = '┃' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked = { text = '┆' },
+          add = { text = "┃" },
+          change = { text = "┃" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
         },
         signs_staged = {
-          add = { text = '┃' },
-          change = { text = '┃' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked = { text = '┆' },
+          add = { text = "┃" },
+          change = { text = "┃" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
         },
         signs_staged_enable = true,
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -108,35 +87,35 @@ return {
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
-          follow_files = true
+          follow_files = true,
         },
         auto_attach = true,
         attach_to_untracked = false,
         current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
           virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
           delay = 1000,
           ignore_whitespace = false,
           virt_text_priority = 100,
           use_focus = true,
         },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+        current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
         max_file_length = 40000, -- Disable if file is longer than this (in lines)
         preview_config = {
           -- Options passed to nvim_open_win
-          style = 'minimal',
-          relative = 'cursor',
+          style = "minimal",
+          relative = "cursor",
           row = 0,
-          col = 1
+          col = 1,
         },
       }
-      require('gitsigns').setup {
+      require("gitsigns").setup {
         on_attach = function(bufnr)
-          local gitsigns = require('gitsigns')
+          local gitsigns = require "gitsigns"
 
           local function map(mode, l, r, opts)
             opts = opts or {}
@@ -145,72 +124,72 @@ return {
           end
 
           -- Navigation
-          map('n', ']c', function()
+          map("n", "]c", function()
             if vim.wo.diff then
-              vim.cmd.normal({ ']c', bang = true })
+              vim.cmd.normal { "]c", bang = true }
             else
-              gitsigns.nav_hunk('next')
+              gitsigns.nav_hunk "next"
             end
           end, { desc = "Go to next Git hunk" })
 
-          map('n', '[c', function()
+          map("n", "[c", function()
             if vim.wo.diff then
-              vim.cmd.normal({ '[c', bang = true })
+              vim.cmd.normal { "[c", bang = true }
             else
-              gitsigns.nav_hunk('prev')
+              gitsigns.nav_hunk "prev"
             end
           end, { desc = "Go to previous Git hunk" })
 
           -- Actions
-          map('n', '<leader>gs', gitsigns.stage_hunk, { desc = "Stage current hunk" })
-          map('n', '<leader>gr', gitsigns.reset_hunk, { desc = "Reset current hunk" })
+          map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage current hunk" })
+          map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset current hunk" })
 
-          map('v', '<leader>gs', function()
-            gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+          map("v", "<leader>gs", function()
+            gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
           end, { desc = "Stage selected hunks in visual mode" })
 
-          map('v', '<leader>gr', function()
-            gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+          map("v", "<leader>gr", function()
+            gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
           end, { desc = "Reset selected hunks in visual mode" })
 
-          map('n', '<leader>gS', gitsigns.stage_buffer, { desc = "Stage entire buffer" })
-          map('n', '<leader>gR', gitsigns.reset_buffer, { desc = "Reset entire buffer" })
-          map('n', '<leader>gp', gitsigns.preview_hunk, { desc = "Preview current hunk" })
-          map('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = "Preview current hunk inline" })
+          map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage entire buffer" })
+          map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset entire buffer" })
+          map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview current hunk" })
+          map("n", "<leader>gi", gitsigns.preview_hunk_inline, { desc = "Preview current hunk inline" })
 
-          map('n', '<leader>gb', function()
-            gitsigns.blame_line({ full = true })
+          map("n", "<leader>gb", function()
+            gitsigns.blame_line { full = true }
           end, { desc = "Show full blame info for current line" })
 
-          map('n', '<leader>gd', gitsigns.diffthis, { desc = "Diff current buffer with HEAD" })
+          map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff current buffer with HEAD" })
 
-          map('n', '<leader>gD', function()
-            gitsigns.diffthis('~')
+          map("n", "<leader>gD", function()
+            gitsigns.diffthis "~"
           end, { desc = "Diff current buffer with HEAD~" })
 
-          map('n', '<leader>gQ', function()
-            gitsigns.setqflist('all')
+          map("n", "<leader>gQ", function()
+            gitsigns.setqflist "all"
           end, { desc = "Set quickfix list with all hunks" })
-          map('n', '<leader>gq', gitsigns.setqflist, { desc = "Set quickfix list with hunks" })
+          map("n", "<leader>gq", gitsigns.setqflist, { desc = "Set quickfix list with hunks" })
 
           -- Toggles
-          map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
-          map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = "Toggle word diff" })
+          map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+          map("n", "<leader>tw", gitsigns.toggle_word_diff, { desc = "Toggle word diff" })
 
           -- Text object
-          map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = "Select current hunk (text object)" })
-        end
+          map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Select current hunk (text object)" })
+        end,
       }
     end,
   },
   {
     -- glance at matched information, include highlight scrollbar
-    'kevinhwang91/nvim-hlslens',
+    "kevinhwang91/nvim-hlslens",
     lazy = false,
     dependencies = { "petertriho/nvim-scrollbar" }, -- ensuring nvim-scrollbar is loaded first
     config = function()
-      require("scrollbar.handlers.search").setup({ -- integrate with nvim-scrollbar
-      })
-    end
-  }
+      require("scrollbar.handlers.search").setup { -- integrate with nvim-scrollbar
+      }
+    end,
+  },
 }
