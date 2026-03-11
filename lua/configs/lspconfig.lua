19d407c9192cc2786507b69b72c0840af3caf10d
sonnk.010 <sonnk.010>
2026-03-11 16:44:54 +0700

    add code actions

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "gopls", "jdtls", "lua_ls"}
+local servers = { "html", "cssls", "pyright", "gopls", "jdtls", "lua_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
+
+local map = vim.keymap.set
+
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
